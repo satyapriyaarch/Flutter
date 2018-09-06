19914bc97be3fe6007893b02b3521375930bd36c
apple <satyapriya.arch@gmail.com>
2018-09-06 13:44:38 +0530

    Ok

--- oca_app/lib/main.dart
+++ oca_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:oca_app/login.dart';
 
 void main() {
   runApp(new MyApp());
 }
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -16,26 +18,34 @@ class MyApp extends StatelessWidget {
         canvasColor: const Color(0xFFfafafa),
         fontFamily: 'Roboto',
       ),
-      home: new MyHomePage(),
+      home: new SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/Login': (BuildContext context) => new LoginScreen(),
+//        '/FSDashboard' : (BuildContext context) => new FSDashboard(),
+//        '/SVDashboard' : (BuildContext context) => new FSDashboard(),
+//        '/TVDashboard' : (BuildContext context) => new FSDashboard(),
+//        '/IPDashboard' : (BuildContext context) => new FSDashboard(),
+//        '/OCADashboard' : (BuildContext context) => new FSDashboard(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _SplashScreenState createState() => new _SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
         padding: const EdgeInsets.all(0.0),
-        child: new Image.asset('images/yellow-pattern.png' ,
-        fit: BoxFit.fitWidth,
-
+        child: new Image.asset(
+          'images/yellow-pattern.png',
+          fit: BoxFit.fitWidth,
         ),
       ),
     );
