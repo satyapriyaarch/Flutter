0e15fde15ea4f1963984bada23a9abcb49802a71
apple <satyapriya.arch@gmail.com>
2018-09-06 17:04:28 +0530

    Ok

--- oca_app/lib/main.dart
+++ oca_app/lib/main.dart
@@ -38,6 +38,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/Login');
+  }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
